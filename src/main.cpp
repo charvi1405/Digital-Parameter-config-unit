0be1a5d2bc0972d921f0f2ddb1347bce48412422
charvi1405 <charvi.mittal_cs.aiml24@gla.ac.in>
2026-02-11 14:06:06 +0530

    show on screen

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,23 @@
 #define SCREEN_HEIGHT 64
 #define OLED_ADDR 0x3C
 
-// Create display object
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1  );
 void setup() {
-    Serial.begin(9600);
-    // Initialize I2C OLED
+    Serial.begin(115200);
     if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
         Serial.println("OLED not found");
         while (true);
     }
     display.clearDisplay();
-
-    // Set text properties
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
-
-    // Display text
     display.setCursor(0, 0);
     display.println("Arduino UNO R4");
     display.println("OLED with I2C");
     display.println("Hello Students!");
-
     display.display();
-
-
 }
 
-void loop() {
-// write your code here
+void loop()
+    // write your code here
 }
