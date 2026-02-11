bacd80385d1fea2413625df3cd74b8a3ab8117a3
charvi1405 <charvi.mittal_cs.aiml24@gla.ac.in>
2026-02-11 13:55:06 +0530

    add display object

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDR 0x3C
+
+// Create display object
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &amp;Wire, -1);
 void setup() {
 // write your initialization code here
 }
